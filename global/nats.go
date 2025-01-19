032d1f51ef679cf948c9ec006a7d7e18f9def421
unknown <arshia.bahar@aut.ac.ir>
2025-01-19 04:30:36 +0330

    fix: fixed some names

--- global/nats.go
+++ global/nats.go
@@ -3,7 +3,7 @@ package global
 const (
 	StreamName           = "og_describer_heroku"
 	JobQueueTopic        = "og_describer_heroku_job_queue"
-	ConsumerGroup        = "describers-heroku"
+	ConsumerGroup        = "describer-heroku"
 	JobQueueTopicManuals = "og_describer_heroku_manuals_job_queue"
-	ConsumerGroupManuals = "describers-heroku-manuals"
+	ConsumerGroupManuals = "describer-heroku-manuals"
 )
